ddf777af5faa4389adaa09e433161c5819c14599
nico <nicolas.roux@aspectize.com>
2020-05-21 18:05:32 +0200

    Init

---
+++ BootstrapSelect/BootstrapSelect/BootstrapSelect.js
@@ -0,0 +1,19 @@
+﻿/* Bootstrap select extension */
+/* Build with https://developer.snapappointments.com/bootstrap-select/ */
+
+
+Aspectize.Extend("BootstrapSelect", {
+    Binding: 'ListBinding',
+    TemplateBuilder: '',
+    Properties: { title: '', className: '', style: '', disabled: '', selectedIndex: '', size: '', hidden: '', SelectedValue: '', CurrentData: '', CurrentValue: '', CurrentDisplay: '', DefaultIndex: '', NullValueDisplay: '', CurrentSyncDisabled: '', DisplayMemberClass: '' },
+    Events: ['SelectionChanged', 'SelectedValueChanged'],
+    Init: function (elem) {
+
+        $(elem).selectpicker({
+            liveSearch: true,
+            maxOptions: 1
+        });
+
+
+    }
+});
